554d6476754155390e7a11209987f666109810c2
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-03-07 12:15:36 -0600

    Add saving and loading of configuration

---
+++ dualgit/Config.fs
@@ -0,0 +1,18 @@
+﻿module DualGit.Config
+
+open FSharp.Configuration
+open System.IO
+
+let configFile = ".dualgit"
+
+type Config = YamlConfig<"sample.yaml">
+
+let get () =
+    if File.Exists configFile then
+        let config = Config()
+        config.Load configFile
+        config |> Some
+    else
+        None 
+
+let save (config: Config) = config.Save configFile
